a776358be6e968f345c1210574de065919f7f678
ningwow <rjn.2085@gmailcom>
2022-12-04 15:36:07 +0800

    12-4

--- fcitx5.sh
+++ fcitx5.sh
@@ -86,4 +86,4 @@ echo '' >> /home/rjn/.xprofile
 
 fcitx5
 echo 'exec --no-startup-id fcitx5' >> /home/rjn/.config/i3/config
-echo ('please:1.stop fcitx5  2.vim .config/fcitx5/conf/pinyin.conf PreeditInApplication=False  ')
+echo 'please:1.stop fcitx5  2.vim .config/fcitx5/conf/pinyin.conf PreeditInApplication=False '
