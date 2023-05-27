a971c08465a0b9b800cb1b13d3a4a2bd7b4a9b84
ningwow <rjn.2085@gmail.com>
2023-05-27 18:10:46 +0800

    23-5-27

--- a/i3lockr.sh
+++ a/i3lockr.sh
@@ -4,5 +4,7 @@
 pacman -S --noconfirm xautolock
 wget -P /usr/locale/bin  https://github.com/owenthewizard/i3lockr/releases/download/v1.2.0/i3lockr
 chmod +x /usr/locale/bin/i3lockr
-echo "exec --no-startup-id xautolock -time 1 -locker 'i3lockr --blur=20'" >> ~/.config/i3/config
+
+#i3配置文件已cp
+#echo "exec --no-startup-id xautolock -time 1 -locker 'i3lockr --blur=20'" >> ~/.config/i3/config
 #手动配置i3lockr --blur=25到i3config
--- a/screenshot.sh
+++ a/screenshot.sh
@@ -3,5 +3,6 @@
 #截图工具
 pacman -S --noconfirm flameshot
 #记得设置flameshot
-echo "bindsym $mod+shift+s exec "flameshot gui"" >> /home/rjn/.config/i3/config
-echo "bindsym Print exec "flameshot gui"" >> /home/rjn/.config/i3/config
+#i3配置文件已cp
+#echo "bindsym $mod+shift+s exec "flameshot gui"" >> /home/rjn/.config/i3/config
+#echo "bindsym Print exec "flameshot gui"" >> /home/rjn/.config/i3/config
