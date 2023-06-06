1010d2a5999350cf7e4bdcce3ea68aef5d05f086
ningwow <rjn.2085@gmail.com>
2023-06-06 15:23:51 +0800

    23-6-6

--- a/mouse.sh
+++ a/mouse.sh
@@ -8,7 +8,7 @@
 
 #停滞5秒后隐藏鼠标
 pacman -S --noconfirm unclutter
-
+unclutter &
 #i3配置文件已复制
 # echo "exec unclutter &" >> /home/rjn/.config/i3/config
 
@@ -26,6 +26,6 @@ pacman -S --noconfirm unclutter
 # 使用方法：https://wiki.archlinux.org/title/Xinput
 # 设置 touchpad 的 accel speed 属性调高为0.3
 # 标识符会改变，名称不会变,所以使用名称
-#eg: xinput set-prop "HTIX5288:00 0905:0255 Touchpad" "libinput Accel Speed" 0.3
+xinput set-prop "HTIX5288:00 0905:0255 Touchpad" "libinput Accel Speed" 0.3
 #eg: xinput set-prop 11 315 0.3
 # 记得加入到/etc/profile启动文件中
