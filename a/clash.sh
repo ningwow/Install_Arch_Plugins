ed23b8151c8ae73a256e37e559061f7f423094d7
ningwow <rjn.2085@gmail.com>
2023-06-29 09:34:04 +0800

    23-6-29

--- a/clash.sh
+++ a/clash.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
+##或从aur安装clash   优先
+paru -S clash-for-windows-bin
+cfw
+echo "exec "sleep 2 && cfw &""
+
+#{{{tun模式参考
+#原理，关闭系统dns服务，使用clash的dns劫持代替。不用创建虚拟网卡
+#https://help.bprolink.com/linux/clash.html
+#首先需要下载 Clash Premium 版本，替换上面的 clash 文件。接着需要设置 Linux 系统，开启转发功能。编辑文件 /etc/sysctl.conf，添加以下内容：
+net.ipv4.ip_forward=1
+
+#保存退出后，执行以下命令使修改生效：
+sudo sysctl -p
+
+#然后接着需要关闭系统的 DNS 服务，使用以下命令：
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+
+#然后启用clash中tun按钮，关闭环境变量}}}
+
+
+
+
+
+
+
+
+
+
+
+
 
 #安装clash
 wget https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.20.22/Clash.for.Windows-0.20.22-x64-linux.tar.gz
--- a/dns/dns.sh
+++ a/dns/dns.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #使用dhcpcd自动生成dns配置文件
 
+#在使用clash的dns劫持服务时为使用tun模式，关闭系统dns服务
+
+
 #在/etc/dhcpcd.conf中配置以下
 echo "#禁用钩子停止覆盖dns,resolv.conf" >> /etc/dhcpcd.conf
 echo "#nohook resolv.conf" >> /etc/dhcpcd.conf
