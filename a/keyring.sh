e257e56e9ff75c66ca783ea99c67925db3f146e3
ningwow <rjn.2085@gmail.com>
2023-08-07 09:47:41 +0800

    23-8-7

--- a/keyring.sh
+++ a/keyring.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-#vscode解决方法，在code启动中(/usr/bin/code)加入 "--password-store="gnome""
+#gnome-keyring i3环境配置(/etc/profile)
+dbus-update-activation-environment --all
+gnome-keyring-daemon --start --components=secrets
+
+
+
+
+#vscode需要指定密钥环，在code启动文件中中(/usr/bin/code)加入 "--password-store="gnome""
 #钥匙环gui
 pacman --noconfirm -S seahorse  #giu管理程序
