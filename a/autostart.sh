97a584a872320efc3b7418a10a26b4cbd717ddf1
ningwow <rjn.2085@gmail.com>
2023-09-03 08:26:09 +0800

    23-9-3

--- a/autostart.sh
+++ a/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #在i3中，先安装dex自动启动程序
+#~/.config/autostart/*    /etc/xdg/autostart/*
 #在autostart中创建.desktop文件并赋予权限
 #.desktop文件参考 https://specifications.freedesktop.org/autostart-spec/autostart-spec-latest.html
 #https://wiki.archlinux.org/title/XDG_Autostart
