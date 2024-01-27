4790ba34f71e6731051e743c1b5d23261d6bba8a
ningwow <rjn.2085@gmail.com>
2024-01-27 13:21:13 +0800

    34-1-27

--- a/hooks/gtk-3.0-config/gtk3.0-config.sh
+++ a/hooks/gtk-3.0-config/gtk3.0-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#pacman的更新log在/var/log/pacman.log中，解决问题请查看log
 #防止每次更新gtk后系统gtk3.0的配置文件被覆盖
 cp gtk-config-custom.hook /usr/share/libalpm/hooks/gtk-config-custom.hook
 cp gtk-config-custom /usr/share/libalpm/scripts/gtk-config-custom
