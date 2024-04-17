94651d0784d4d896fe64ea1e4f938af11693575f
ningwow <rjn.2085@gmail.com>
2024-04-18 00:23:31 +0800

    24-4-17

--- a/importent-polkit.sh
+++ a/importent-polkit.sh
@@ -9,3 +9,8 @@
 #ked的每运行一次必须重新运行程序
 #将安装的程序路径
 #通过在i3配置文件中配置自启动 exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+
+#使用此执行文件在/usr/bin/polkit-gnome 下执行
+#!/bin/bash
+# /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+~
