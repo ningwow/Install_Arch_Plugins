97dab1fb3fe805fbf7bd4170a98001242abd1b26
ningwow <rjn.2085@gmail.comm>
2024-04-22 10:31:16 +0800

    24-04-22

--- 2.install_arch-new-2.sh
+++ 2.install_arch-new-2.sh
@@ -22,7 +22,7 @@ echo "First" >> /etc/hostname   #主机名
 #设置host文件
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 First" >> /etc/hosts
+echo "127.0.0.1 First" >> /etc/hosts
 
 #初始化文件
 mkinitcpio -P
