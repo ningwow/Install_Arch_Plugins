86329062b20a0a64abb89bd38b305c9c6b049503
ningwow <rjn.2085@gmail.com>
2024-05-19 20:33:55 +0800

    24-5-19

--- Plugins/alacritty/alacritty.sh
+++ Plugins/alacritty/alacritty.sh
@@ -1,15 +1,16 @@
-#使用kitty
-kitty
-#使用以下命令切换主题
-kitty +kitten themes
-Dark *
+# #使用kitty
+# kitty
+# #使用以下命令切换主题
+# kitty +kitten themes
+# Dark *
 
 #!/bin/bash
 sudo pacman -S --noconfirm alacritty
 #配置文件难找
 su rjn -c "mkdir -p /home/rjn/.config/alacritty/"
-su rjn -c "cp alacritty.yml > /home/rjn/.config/alacritty/alacritty.yml"
-
+su rjn -c "cp alacritty/alacritty.toml > /home/rjn/.config/alacritty/alacritty.toml"
+# 主题颜色配置
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 #手动配置
 # 颜色配置参考 https://github.com/alacritty/alacritty-theme
